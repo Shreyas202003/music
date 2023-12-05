be188f07ebb1cb4141c4bc87d4db8d7ed9b00bd2
Shreyas202003 <shreyassalunkhe2003@gmail.com>
2023-12-05 13:10:33 +0530

    music

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-$_SESSION['id']="";     //The give email id should get null to start for new user
-session_destroy();          
-header("location:login-htm.php");       //It will direct to the login page
-?>
